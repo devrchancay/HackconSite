d61c9a87e9266dcb19c81399a3bdec2364a68f69
Ramon Chancay <ramon.chancay@gmail.com>
2015-06-23 23:25:19 -0500

    clases

--- public/js/itemComponent.js
+++ public/js/itemComponent.js
@@ -1,21 +1,46 @@
 var ItemComponent = React.createClass({displayName: "ItemComponent",
+    getInitialState : function(){
+  		return {
+  			charlas : []
+  		}
+	},
+  componentDidMount : function() {
+    $.getJSON('clases.json' , function(result){
+      var charlas = result;
+      if(this.isMounted())
+      {
+        this.setState({
+          charlas : charlas
+        });
+      }
+    }.bind(this));
+  },
    render : function(){
+      var charlas = this.state.charlas;
      return (
-       React.createElement("div", {className: "col s12 m12 l8 offset-l2"},
-         React.createElement("div", {className: "card-panel grey lighten-5 z-depth-1"},
-           React.createElement("div", {className: "row valign-wrapper"},
-             React.createElement("div", {className: "col s4 m2"},
-               React.createElement("img", {src: "https://pbs.twimg.com/profile_images/605493812060749825/q2wRBwtL_400x400.png", alt: "", className: "circle responsive-img"})
-             ),
-             React.createElement("div", {className: "col s8 m10"},
-               React.createElement("span", {className: "black-text"},
-                React.createElement("h5", {className: "center-text"}, "Ramón chancay - @r_chancay"),
-                React.createElement("h5", {className: "flow-text center-text"}, "Clase #4 (final) de Introducción a Nodejs"),
-                React.createElement("h5", {className: "flow-text center-text"}, "11am - 1pm")
-               )
-             )
-           )
-         )
+       React.createElement("div", null, 
+        
+          charlas.map(function(charla){
+              return (
+                React.createElement("div", {className: "col s12 m12 l8 offset-l2"}, 
+                  React.createElement("div", {className: "card-panel grey lighten-5 z-depth-1"}, 
+                    React.createElement("div", {className: "row valign-wrapper"}, 
+                      React.createElement("div", {className: "col s4 m2"}, 
+                        React.createElement("img", {src: charla.avatar, alt: "", className: "circle responsive-img"})
+                      ), 
+                      React.createElement("div", {className: "col s8 m10"}, 
+                        React.createElement("span", {className: "black-text"}, 
+                         React.createElement("h5", {className: "center-text"}, charla.nombre, " - ", charla.twitter), 
+                         React.createElement("h5", {className: "flow-text center-text"}, charla.titulo), 
+                         React.createElement("h5", {className: "flow-text center-text"}, charla.hora)
+                        )
+                      )
+                    )
+                  )
+                )
+              )
+          })
+        
        )
      )
    }
--- public/js/modules/itemComponent.jsx
+++ public/js/modules/itemComponent.jsx
@@ -4,23 +4,43 @@ var ItemComponent = React.createClass({
   			charlas : []
   		}
 	},
+  componentDidMount : function() {
+    $.getJSON('clases.json' , function(result){
+      var charlas = result;
+      if(this.isMounted())
+      {
+        this.setState({
+          charlas : charlas
+        });
+      }
+    }.bind(this));
+  },
    render : function(){
+      var charlas = this.state.charlas;
      return (
-       <div className="col s12 m12 l8 offset-l2">
-         <div className="card-panel grey lighten-5 z-depth-1">
-           <div className="row valign-wrapper">
-             <div className="col s4 m2">
-               <img src="https://pbs.twimg.com/profile_images/605493812060749825/q2wRBwtL_400x400.png" alt="" className="circle responsive-img" />
-             </div>
-             <div className="col s8 m10">
-               <span className="black-text">
-                <h5 className="center-text">Ramón chancay - @r_chancay</h5>
-                <h5 className="flow-text center-text">Clase #4 (final) de Introducción a Nodejs</h5>
-                <h5 className="flow-text center-text">11am - 1pm</h5>
-               </span>
-             </div>
-           </div>
-         </div>
+       <div>
+        {
+          charlas.map(function(charla){
+              return (
+                <div className="col s12 m12 l8 offset-l2">
+                  <div className="card-panel grey lighten-5 z-depth-1">
+                    <div className="row valign-wrapper">
+                      <div className="col s4 m2">
+                        <img src={charla.avatar} alt="" className="circle responsive-img" />
+                      </div>
+                      <div className="col s8 m10">
+                        <span className="black-text">
+                         <h5 className="center-text">{charla.nombre} - {charla.twitter}</h5>
+                         <h5 className="flow-text center-text">{charla.titulo}</h5>
+                         <h5 className="flow-text center-text">{charla.hora}</h5>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+          })
+        }
        </div>
      )
    }
